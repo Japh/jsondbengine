abc27b28397f62f18e0d4a5975d909592219cf88
japhie@gmail.com <japhie@gmail.com@824a6738-e311-b1b3-a96c-7f3ddeec597f>
2010-05-11 16:41:03 +0000

    Working on making class extend PDO

--- trunk/class.jsondbengine.php
+++ trunk/class.jsondbengine.php
@@ -1,133 +1,133 @@
 <?php
 /*
- * @name JSONDB
- * @params host, username, password, database
- * Class to connect to MySQL database and return results as JSON.
+ * @name jsondbengine
+ * @params dsn, username, password
+ * Class to connect to database via PDO and return results as JSON.
  */
-class jsondbengine
+class jsondbengine extends PDO
 {
-    /* Private variables */
-    private $mysqli;
-    private $result;
-    
-    /* Public variables */
-    public $query;
-    public $rows;
-    public $lastID;
-    public $last_error;
-    
-    /*
-     * __construct
-     * @params host, username, password, database
-     * Initiates new MySQLi connection.
-     */
-    function __construct ($host, $user, $pass, $dbname)
+  /* Private variables */
+  private $mysqli;
+  private $result;
+  
+  /* Public variables */
+  public $query;
+  public $rows;
+  public $lastID;
+  public $last_error;
+  
+  /*
+   * __construct
+   * @params dsn, username, password
+   * Initiates new database connection.
+   */
+  function __construct ($dsn, $user, $pass)
+  {
+    parent::__construct($dsn, $user, $pass);
+    $this->last_error = null;
+    $this->mysqli = new mysqli($host, $user, $pass, $dbname);
+    if (mysqli_connect_errno())
     {
-        $this->last_error = null;
-        $this->mysqli = new mysqli($host, $user, $pass, $dbname);
-        if (mysqli_connect_errno())
-        {
-            $this->throw_error("Connect failed: " . mysqli_connect_error() . "\n");
-        }
+      $this->throw_error("Connect failed: " . mysqli_connect_error() . "\n");
+    }
+  }
+  
+  public function run ($query, $args)
+  {
+    $this->query = $query;
+  }
+  
+  /*
+   * exec
+   * @params query
+   * Executes the given query.
+   */
+  public function q($query = '')
+  {
+    $this->query = $query;
+    
+    if (empty($this->query))
+    {
+      return false;
     }
     
-    public function run ($query, $args)
+    $this->result = $this->mysqli->query($this->query);
+    if (!$this->result)
     {
-        $this->query = $query;
-        
+      $this->throw_error('Query failed.');
+    }
+    $this->rows = $this->mysqli->affected_rows;
+    if (stripos($this->query, 'INSERT') !== false)
+    {
+      $this->lastID = $this->mysqli->insert_id;
+    }
+  }
+  
+  /*
+   * exec
+   * @params query
+   * Executes the given query.
+   */
+  public function exec($query = '')
+  {
+    $this->query = $query;
+    
+    if (empty($this->query))
+    {
+      return false;
     }
     
-    /*
-     * exec
-     * @params query
-     * Executes the given query.
-     */
-    public function q($query = '')
+    $this->result = $this->mysqli->query($this->query);
+    if (!$this->result)
     {
-        $this->query = $query;
-        
-        if (empty($this->query))
-        {
-            return false;
-        }
-        
-        $this->result = $this->mysqli->query($this->query);
-        if (!$this->result)
-        {
-            $this->throw_error('Query failed.');
-        }
-        $this->rows = $this->mysqli->affected_rows;
-        if (stripos($this->query, 'INSERT') !== false)
-        {
-            $this->lastID = $this->mysqli->insert_id;
-        }
+      $this->throw_error('Query failed.');
     }
-    
-    /*
-     * exec
-     * @params query
-     * Executes the given query.
-     */
-    public function exec($query = '')
+    $this->rows = $this->mysqli->affected_rows;
+    if (stripos($this->query, 'INSERT') !== false)
     {
-        $this->query = $query;
-        
-        if (empty($this->query))
-        {
-            return false;
-        }
-        
-        $this->result = $this->mysqli->query($this->query);
-        if (!$this->result)
-        {
-            $this->throw_error('Query failed.');
-        }
-        $this->rows = $this->mysqli->affected_rows;
-        if (stripos($this->query, 'INSERT') !== false)
-        {
-            $this->lastID = $this->mysqli->insert_id;
-        }
+      $this->lastID = $this->mysqli->insert_id;
     }
-    
-    /*
-     * fetch
-     * @params type
-     * Fetches the results from a previously run query.
-     */
-    public function fetch($type = 'object')
+  }
+  
+  /*
+   * fetch
+   * @params type
+   * Fetches the results from a previously run query.
+   */
+  public function fetch($type = 'object')
+  {
+    switch ($type)
     {
-        switch ($type)
-        {
-            case 'array':
-                return $this->result->fetch_array();
-            case 'assoc':
-                return $this->result->fetch_assoc();
-            case 'row':
-                return $this->result->fetch_row();
-            case 'object':
-                return $this->result->fetch_object();
-            default:
-                return false;
-        }
+      case 'array':
+        return $this->result->fetch_array();
+      case 'assoc':
+        return $this->result->fetch_assoc();
+      case 'row':
+        return $this->result->fetch_row();
+      case 'object':
+        return $this->result->fetch_object();
+      default:
+        return false;
     }
-    
-    /*
-     * db_error
-     * @params msg
-     * Outputs MySQL error information including the passed message.
-     */
-    public function throw_error($msg = '')
+  }
+  
+  /*
+   * db_error
+   * @params msg
+   * Outputs MySQL error information including the passed message.
+   */
+  public function throw_error($msg = '')
+  {
+    $this->last_error = $msg;
+    if (empty($this->suppress_errors))
     {
-        $this->last_error = $msg;
-        if (empty($this->suppress_errors))
-        {
-            $output = '';
-            $output .= '<p><strong>Database error</strong><br />';
-            $output .= '<em>Message:</em> ' . $msg . '<br />';
-            $output .= '<em>Database:</em> ' . $this->config['dbname'] . '<br />';
-            $output .= '<em>Query:</em> ' . $this->query . '<br />';
-            $output .= '<em>Error:</em> ' . ($this->mysqli->connect_error ? $this->mysqli->connect_error : $this->mysqli->error) . '</p>';
-            exit($output);
-        }
+      $output = '';
+      $output .= '<p><strong>Database error</strong><br />';
+      $output .= '<em>Message:</em> ' . $msg . '<br />';
+      $output .= '<em>Database:</em> ' . $this->config['dbname'] . '<br />';
+      $output .= '<em>Query:</em> ' . $this->query . '<br />';
+      $output .= '<em>Error:</em> ' . ($this->mysqli->connect_error ? $this->mysqli->connect_error : $this->mysqli->error) . '</p>';
+      exit($output);
     }
+  }
 }
